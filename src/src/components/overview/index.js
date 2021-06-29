f4e549138cb39cdf5e19a007066cf08fdc6da0f8
Lucas Matheus <lucasmpa16@gmail.com>
2021-06-29 11:54:12 -0300

    Clear Terminal Warnings

--- src/src/components/overview/index.js
+++ src/src/components/overview/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { 
   Container,
   FeaturedPlaylists, 
@@ -46,10 +46,10 @@ function Overview() {
           </span>
           <div> 
             <div onClick={() => {setLinguageForTranslate('portuguese')}}>
-              <img src={BrazilFlag} width="40px"/>
+              <img src={BrazilFlag} alt="Brazil Flag" width="40px"/>
             </div>
             <div onClick={() => {setLinguageForTranslate('english')}}>
-              <img src={EuaFlag} width="40px"/>
+              <img src={EuaFlag} alt="EUA Flag" width="40px"/>
             </div>
           </div>
          
@@ -58,35 +58,35 @@ function Overview() {
         <h1>{toTranslate('featuredPlaylists')}</h1>
         <ScrollableContent>
           <div>
-            <img src={Playlist1} alt='Playlist Image'/>
+            <img src={Playlist1} alt='Playlist Content'/>
             <div>
               <h2>Only STARDUST</h2>
               <h5>10 {toTranslate('songs')}</h5>
             </div> 
           </div>
           <div>
-            <img src={Playlist2} alt='Playlist Image'/>
+            <img src={Playlist2} alt='Playlist Content'/>
             <div>
               <h2>Mike Vella</h2>
               <h5>25 {toTranslate('songs')}</h5>
             </div> 
           </div>
           <div>
-            <img src={Playlist3} alt='Playlist Image'/>
+            <img src={Playlist3} alt='Playlist Content'/>
             <div>
               <h2>The Best Singles</h2>
               <h5>09 {toTranslate('songs')}</h5>
             </div> 
           </div>
           <div>
-            <img src={Playlist4} alt='Playlist Image'/>
+            <img src={Playlist4} alt='Playlist Content'/>
             <div>
               <h2>Indie</h2>
               <h5>120 {toTranslate('songs')}</h5>
             </div> 
           </div>
           <div>
-            <img src={Track4} alt='Playlist Image'/>
+            <img src={Track4} alt='Playlist Content'/>
             <div>
               <h2>Rock Hits</h2>
               <h5>20 {toTranslate('songs')}</h5>
@@ -98,21 +98,21 @@ function Overview() {
        <ProfileView>
           <h2>{toTranslate('profileViews')}</h2>
           <div>
-            <img src={View1}/>
+            <img src={View1} alt="View"/>
             <div>
               <h4>Jenny Villa</h4>
               <h5>1 {toTranslate('day')} {toTranslate('ago')}</h5>
             </div>            
           </div>
           <div>
-            <img src={View2}/>
+            <img src={View2} alt="View"/>
             <div>
               <h4>Jenny Villa</h4>
               <h5>4 {toTranslate('days')} {toTranslate('ago')}</h5>
             </div>            
           </div>
           <div>
-            <img src={View3}/>
+            <img src={View3} alt="View"/>
             <div>
               <h4>Aquila</h4>
               <h5>2 {toTranslate('weeks')} {toTranslate('ago')}</h5>
@@ -123,42 +123,42 @@ function Overview() {
           <h2>{toTranslate('tracks')}</h2>
           <TracksContent>
           <div>
-            <img src={Track1} alt='Playlist Image'/>
+            <img src={Track1} alt='Playlist Content'/>
             <div>
               <h2>Mahara Bon</h2>
               <h5>Sexy In Paris</h5>
             </div> 
           </div>
           <div>
-            <img src={Track2} alt='Playlist Image'/>
+            <img src={Track2} alt='Playlist Content'/>
             <div>
               <h2>Donna</h2>
               <h5>Pseudo-life</h5>
             </div> 
           </div>
           <div>
-            <img src={Track3} alt='Playlist Image'/>
+            <img src={Track3} alt='Playlist Content'/>
             <div>
               <h2>Avven</h2>
               <h5>Magic</h5>
             </div> 
           </div>
           <div>
-            <img src={Track4} alt='Playlist Image'/>
+            <img src={Track4} alt='Playlist Content'/>
             <div>
               <h2>Cony Matt</h2>
               <h5>Living in hell</h5>
             </div> 
           </div>
           <div>
-            <img src={Track5} alt='Playlist Image'/>
+            <img src={Track5} alt='Playlist Content'/>
             <div>
               <h2>Claudia Cany</h2>
               <h5>Remember</h5>
             </div> 
           </div>
           <div>
-            <img src={Track6} alt='Playlist Image'/>
+            <img src={Track6} alt='Playlist Content'/>
             <div>
               <h2>Pacify</h2>
               <h5>Portrait</h5>
